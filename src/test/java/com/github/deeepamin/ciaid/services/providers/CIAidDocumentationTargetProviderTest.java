1cad47f068586383a45c7c1915ba2492b2c1e413
Deep Amin <deepamin1994@gmail.com>
2025-05-09 11:29:54 +0200

    add test for quick documentation for inputs

---
+++ src/test/java/com/github/deeepamin/ciaid/services/providers/CIAidDocumentationTargetProviderTest.java
@@ -0,0 +1,59 @@
+package com.github.deeepamin.ciaid.services.providers;
+
+import com.github.deeepamin.ciaid.BaseTest;
+import com.github.deeepamin.ciaid.model.InputDocumentationTarget;
+import com.intellij.platform.backend.documentation.DocumentationData;
+import com.intellij.platform.backend.documentation.PsiDocumentationTargetProvider;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+
+@SuppressWarnings({"UnstableApiUsage", "Overrides"})
+public class CIAidDocumentationTargetProviderTest extends BaseTest {
+  public void testInputElementShowsDocumentation() {
+    myFixture.configureByText(".gitlab-ci.yml", """
+            spec:
+              inputs:
+                stage:
+                  description: The stage the job will run in.
+                  type: string
+                  default: build
+            ---
+            build-dev:
+              needs:
+                - $[[ inputs.stage<caret> ]]
+          """);
+    PsiFile file = myFixture.getFile();
+    int offset = myFixture.getCaretOffset();
+    PsiElement leafPsiElement = myFixture.getFile().findElementAt(offset);
+    assertNotNull(leafPsiElement);
+    PsiElement element = leafPsiElement.getParent();
+
+    var providers = PsiDocumentationTargetProvider.EP_NAME.getExtensionList();
+    assertNotNull(providers);
+    assertEquals(1, providers.size());
+    var documentationTargetProvider = providers.getFirst();
+    assertTrue(documentationTargetProvider instanceof CIAidDocumentationTargetProvider);
+    var ciAidDocumentationTargetProvider = (CIAidDocumentationTargetProvider) documentationTargetProvider;
+
+    var documentationTarget = ciAidDocumentationTargetProvider.documentationTarget(file, element);
+    assertNotNull(documentationTarget);
+    assertTrue(documentationTarget instanceof InputDocumentationTarget);
+    var inputDocumentationTarget = (InputDocumentationTarget) documentationTarget;
+
+    var presentation = inputDocumentationTarget.computePresentation();
+    assertNotNull(presentation);
+    assertEquals("stage Documentation", presentation.getPresentableText());
+    assertEquals("/src/.gitlab-ci.yml", presentation.getLocationText());
+    assertNotNull(presentation.getLocationIcon());
+
+    var documentationResult = inputDocumentationTarget.computeDocumentation();
+    var expectedHtml = "<div style=\"font-family: JetBrains Mono; font-weight: bold; margin-bottom: 5px;\">stage: string</div>" +
+                       "<hr/>" +
+                       "<div style=\"margin-top: 5px; margin-bottom: 5px;\">The stage the job will run in.</div>" +
+                       "<span style=\"color: gray; margin-bottom: 8px;\">default: build</span>";
+    assertNotNull(documentationResult);
+    assertTrue(documentationResult instanceof DocumentationData);
+    var documentationData = (DocumentationData) documentationResult;
+    assertEquals(expectedHtml, documentationData.getHtml());
+  }
+}
